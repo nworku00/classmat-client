1bb6a73b51725071daf2b5d6b30fd51e78a65d63
Nati Worku <natiworku1971@gmail.com>
2023-04-17 09:19:36 -0400

    initial 2

--- script.js
+++ script.js
@@ -7,21 +7,78 @@ let numPian = ''
 let drum = document.querySelector('.drum')
 let bass = document.querySelector('.bass')
 let pian = document.querySelector('.pian')
-let generator = document.querySelector('.gene')
-//
-//function selectValue() {
-//     
-//     outputDrum = drum.options[drum.selectedIndex].value;
-//     
-//     outputBass = bass.options[bass.selectedIndex].value;
-//     
-//     outputPian = pian.options[pian.selectedIndex].value;
-// }
+let generator = document.querySelector('.generator')
+
 let call = () => {
+    function selectValue() {
+    
+    outputDrum = drum.options[drum.selectedIndex].value;
+
+    outputBass = bass.options[bass.selectedIndex].value;
+    
+    outputPian = pian.options[pian.selectedIndex].value;
+}
+    selectValue()
+    fetch(`${url}/drummers`)
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i <= parseInt(outputDrum); i++){
+            
+            let drumHtml= `
+            <div class="card" style="width: 18rem;">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${data[i].instrument}</li>
+                    <li class="list-group-item">${data[i].name}</li>
+                </ul>
+            </div>
+            `
+            let sec = document.createElement("div");
+            sec.classList.add('col')
+            sec.innerHTML = drumHtml;
+            generator.append(sec);
+        } 
+    }).catch(err => {
+        console.log(err)
+    })
     fetch(`${url}/bassists`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        for (let i = 0; i <= parseInt(outputBass); i++){
+            
+            let bassHtml= `
+            <div class="card" style="width: 18rem;">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${data[i].instrument}</li>
+                    <li class="list-group-item">${data[i].name}</li>
+                </ul>
+            </div>
+            `
+            let sec = document.createElement("div");
+            sec.classList.add('col')
+            sec.innerHTML = bassHtml;
+            generator.append(sec);
+        } 
+    }).catch(err => {
+        console.log(err)
+    })
+    fetch(`${url}/pianists`)
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i <= parseInt(outputPian); i++){
+            
+            let pianHtml= `
+            <div class="card" style="width: 18rem;">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${data[i].instrument}</li>
+                    <li class="list-group-item">${data[i].name}</li>
+                </ul>
+            </div>
+            `
+            let sec = document.createElement("div");
+            sec.classList.add('col')
+            sec.innerHTML = pianHtml;
+            generator.append(sec);
+        } 
     }).catch(err => {
         console.log(err)
     })
